4021565ade9e0fecc5794fdbf572e0b40c1fb04e
Julian Hochstetter <julian.hochstetter@mni.thm.de>
2012-08-16 09:43:58 +0200

    make plugin ready for redmine 2.0

--- init.rb
+++ init.rb
@@ -1,9 +1,7 @@
 require 'redmine'
 
-# Patches to the Redmine core.
-require 'dispatcher'
 
-Dispatcher.to_prepare :redmine_hide_email_from_public do
+ActionDispatch::Callbacks.to_prepare do 
   require_dependency 'user_preference'
   unless UserPreference.included_modules.include? HideEmailFromPublic::Patches::UserPreferencePatch
     UserPreference.send(:include, HideEmailFromPublic::Patches::UserPreferencePatch)
